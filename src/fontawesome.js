fb69fc8cb022071b61482a847c405b57d59eebd7
armandasp <armandaspilk@gmail.com>
2021-11-28 21:34:48 +0200

    fontawesome added

---
+++ src/fontawesome.js
@@ -0,0 +1,12 @@
+// import the library
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+// import your icons
+import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
+
+library.add(
+  faTrashAlt
+  // more icons go here
+);
+
+export default library;

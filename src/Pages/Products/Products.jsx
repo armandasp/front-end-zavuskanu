c102ef679de60ec45623805a8200f066ca300cf7
armandasp <armandaspilk@gmail.com>
2021-11-28 15:03:36 +0200

    .

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Nav, Block } from "../../Components";
 import * as S from "./Products.styles";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../../Contexts/Auth";
 
 const links = [
@@ -67,53 +67,7 @@ const Products = () => {
                   }
                   alert(`${e.target.title} pridėtas į krepšelį`);
                 });
-
-              // items.forEach((item) => {
-              //   console.log(item.id === Number(e.target.id));
-              //   if (item.id === Number(e.target.id)) {
-              //     fetch(`http://localhost:3000/v1/carts/add/${item.id}`, {
-              //       method: "POST",
-              //       headers: {
-              //         authorization: `Bearer ${authContext.token}`,
-              //         "Content-Type": "application/json",
-              //       },
-              //       body: JSON.stringify(item),
-              //     })
-              //       .then((res) => res.json())
-              //       .then((data) => {
-              //         if (!data.msg) {
-              //           alert(data.err);
-              //         }
-              //         alert(`${item.title} pridėtas į krepšelį`);
-              //       });
-              //   }
-              //   alert("something is wrong");
-              // });
             }}
-            // handleClick={(e) => {
-            //   e.preventDefault();
-            //   console.log(e.target.id);
-            // }}
-
-            //   e.preventDefault();
-            // fetch(`http://localhost:3000/v1/carts/add/${id}`, {
-            //   method: "POST",
-            //   headers: {
-            //     authorization: `Bearer ${authContext.token}`,
-            //     "Content-Type": "application/json",
-            //   },
-            //   body: JSON.stringify(items),
-            // })
-            //   .then((res) => res.json())
-            //   .then((data) => {
-            //     if (!data.msg) {
-            //       alert(data.err);
-            //       return console.log(data.err);
-            //     }
-            //     alert(data.msg);
-            //   })
-            //   .catch((err) => alert(err));
-            // }}
           />
         </S.SectionStyle>
       )}

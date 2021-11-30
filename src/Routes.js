197d49fe17ec2fa8ef7674e2507bb95ef9e282b2
armandasp <armandaspilk@gmail.com>
2021-11-30 21:45:54 +0200

    lazyloading

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Products, Sets, Register, Login, Carts } from "./Pages";
+import { Products, Sets, Register, Login } from "./Pages";
+
+const LazyCarts = React.lazy(() => import("./Pages/Carts/Carts"));
 
 const PageRouter = () => {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/" element={<Products />} />
-        <Route exact path="/sets" element={<Sets />} />
-        <Route exact path="/carts" element={<Carts />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<Login />} />
-      </Routes>
+      <Suspense fallback={() => <h1>Loading...</h1>}>
+        <Routes>
+          <Route exact path="/" element={<Products />} />
+          <Route exact path="/sets" element={<Sets />} />
+          <Route exact path="/carts" element={<LazyCarts />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };

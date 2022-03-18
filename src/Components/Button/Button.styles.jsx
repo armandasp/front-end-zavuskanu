216869bfcc494c6f602aace92ca6cbde04eca0c6
armandasp <armandaspilk@gmail.com>
2022-03-18 23:45:52 +0200

    hover effect

--- src/Components/Button/Button.styles.jsx
+++ src/Components/Button/Button.styles.jsx
@@ -6,6 +6,13 @@ export const Button = styled.button`
   padding: 0.5rem 1rem;
   cursor: pointer;
   color: #fff;
+  :hover {
+    background: #fff;
+    color: #4d4d4d;
+    /* transition: 0.2s; */
+    border: 0.1rem solid #4d4d4d;
+    padding: 0.4rem 0.9rem;
+  }
 
   @media screen and (max-width: 640px) {
     font-size: 0.8rem;
